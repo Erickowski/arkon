fc05cd2a6a524b4ea9b301ac6fd8676077ae3ffd
Erickowski <daniel_perez_03081995@hotmail.com>
2020-10-27 10:41:18 -0600

    Syles for header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,43 @@
 import React from "react";
 import styled from "@emotion/styled";
 
-const headerContainer = styled.header`
-  width: 100vw;
+const HeaderContainer = styled.header`
   display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  width: 100vw;
+  margin: 0;
+  padding: 1rem;
+  background-color: var(--blue);
+  color: var(--white);
+  ul {
+    list-style: none;
+  }
+  @media (min-width: 768px) {
+    flex-direction: row;
+    justify-content: space-between;
+
+    ul li {
+      display: inline-block;
+    }
+  }
 `;
 
 const Header = () => {
-  return <headerContainer>Aplicacion de productividad</headerContainer>;
+  return (
+    <HeaderContainer>
+      <h1>Aplicacion de productividad</h1>
+      <nav>
+        <ul>
+          <li>Nueva tarea</li>
+          <li>Historial de tareas completadas</li>
+          <li>Prellenar tareas</li>
+        </ul>
+      </nav>
+    </HeaderContainer>
+  );
 };
 
 export default Header;
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { css, Global } from "@emotion/core";
 
 import Footer from "./Footer";
 import Header from "./Header";
@@ -6,6 +7,42 @@ import Header from "./Header";
 const Layout = ({ children }) => {
   return (
     <>
+      <Global
+        styles={css`
+          :root {
+            --blue: #031d44;
+            --white: #f1f1f1;
+            --black: #020202;
+            --font: "Roboto", sans-serif;
+          }
+          html {
+            font-size: 62.5%;
+            box-sizing: border-box;
+          }
+          *,
+          *:before,
+          *:after {
+            box-sizing: inherit;
+          }
+          body {
+            font-size: 1.6rem;
+            line-height: 1.5;
+            margin: 0;
+            background-color: var(--white);
+            font-family: var(--font);
+          }
+          h1,
+          h2,
+          h3 {
+            margin: 0;
+            padding: 0;
+          }
+          ul {
+            margin: 0;
+            padding: 0;
+          }
+        `}
+      />
       <Header />
       {children}
       <Footer />
